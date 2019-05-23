c3504d3f0410506697c6c64290cfff8ff550750a
Josh Cook <joshuacook0023@gmail.com>
2019-05-23 16:45:00 -0400

    feat: select either mutual exclusivity or co-mutation test

--- R/rc_test.R
+++ R/rc_test.R
@@ -1,19 +1,22 @@
-#' The Row-Column-Exclusivity test
+#' The Row-Column-Exclusivity or Co-mutation test
 #'
-#' @description The Row-Column-Exclusivity test cannot be calculated exactly for
-#'   most use cases because there is no closed formula and a very large number
-#'   of possible matrices to consider. Thus, the probabilities are calculated
-#'   empirically from a sufficiently large number of samples of possible
-#'   matrices.
+#' @description The Row-Column-Exclusivity or Co-mutation test cannot be
+#'   calculated exactly for most use cases because there is no closed formula
+#'   and a very large number of possible matrices to consider. Thus, the
+#'   probabilities are calculated empirically from a sufficiently large number
+#'   of samples of possible matrices.
 #'
 #' @param dat tibble with mutation information
 #' @param sample_col column of samples names (quoted)
 #' @param mutgene_col column of genes that are mutated (quoted)
 #' @param k size of gene sets to consider (default is 2)
-#' @param seed_genes a vector of gene(s) that must be in the gene set to be tested (optional)
+#' @param which_test test for mutual exclusivity (\code{"exclusivity"}) or
+#'   co-mutation (\code{"comutation"}); (default is \code{"comutation"})
+#' @param seed_genes a vector of gene(s) that must be in the gene set to be
+#'   tested (optional)
 #' @param N_perms number of permutation matrices to use (default is 10,000)
-#' @param min_mutex_events minimum number of real mutual exclusive events required to
-#'   consider the gene set (default is 2)
+#' @param min_mutex_events minimum number of real mutual exclusive events
+#'   required to consider the gene set (default is 2)
 #'
 #' @examples
 #' library(wext)
@@ -25,9 +28,20 @@
 #' @export rc_test
 rc_test <- function(dat, sample_col, mutgene_col,
                     k = 2,
+                    which_test = c("exclusivity", "comutation"),
                     seed_genes = c(),
                     N_perms = 1e4,
                     min_mutex_events = 2) {
+
+    # choose only one test
+    which_test <- stringr::str_to_lower(which_test[[1]])
+    if (which_test %in% c("exclusivity", "e")) {
+        test_func <- calc_mutex_events
+    } else if (which_test %in% c("comutation", "c")) {
+        test_func <- calc_comut_events
+    } else {
+        stop(paste(which_test, "is not an option."))
+    }
 
     # enquote the input column names
     sample_col <- rlang::enquo(sample_col)
@@ -48,22 +62,20 @@ rc_test <- function(dat, sample_col, mutgene_col,
 
     results_tib <- make_empty_results_tracker(genes, k, seed_genes = seed_genes) %>%
         dplyr::mutate(
-            t_AM = purrr::map_dbl(gene_sets, calc_mutex_events, bgr = bipartite_gr)
+            t_AM = purrr::map_dbl(gene_sets, test_func, bgr = bipartite_gr)
         ) %>%
         dplyr::filter(t_AM > !!min_mutex_events)
     if (nrow(results_tib) < 1) {
         stop("No gene sets to test that pass the minumum number of real mutaully exclusvie events")
     }
 
-    cat("permutation: ")
     for (i in 1:N_perms) {
-        cat(i, " ")
         perm_bgr <- bipartite_edge_swap(bipartite_gr, Q = 100)
         results_tib <- purrr::pmap_df(
-            results_tib, update_results_tib, bgr = perm_bgr
+            results_tib, update_results_tib, f = test_func, bgr = perm_bgr
         )
     }
-    cat("\n")
+
     results_tib <- results_tib %>%
         dplyr::mutate(p_val = t_BM_ge / N_perms)
     return(results_tib)
@@ -95,21 +107,9 @@ make_empty_results_tracker <- function(gs, k, seed_genes) {
 
 
 
-# calculate the number of mutually exclusive events
-calc_mutex_events <- function(gs, bgr, sample_is = TRUE) {
-    gr <- unlist(gs)
-    mutex_bgr <- bgr %N>%
-        tidygraph::filter(type == !!sample_is | name %in% !!gs) %>%
-        tidygraph::filter(type != !!sample_is |
-                          tidygraph::centrality_degree(mode = "all") == 1)
-    igraph::ecount(mutex_bgr)
-}
-
-
-
 # update the gene sets in the results tracking tibble using the edge-swapped bgr
-update_results_tib <- function(gene_sets, t_BM_ge, t_AM, bgr) {
-    t_BM <- calc_mutex_events(gene_sets, bgr)
+update_results_tib <- function(gene_sets, t_BM_ge, t_AM, f, bgr) {
+    t_BM <- f(gene_sets, bgr)
     if (t_BM >= t_AM) {
         t_BM_ge <- t_BM_ge + 1
     }
--- tests/testthat/test-rc_test.R
+++ tests/testthat/test-rc_test.R
@@ -1,17 +1,22 @@
-test_that("the RC-test works", {
+test_that("the RC-Exclusivity test works", {
     library(dplyr)
     dat <- tibble::tribble(
-        ~samples, ~genes,
-        "A", "gene1",
-        "A", "gene2",
-        "B", "gene1",
-        "B", "gene3",
-        "C", "gene3"
+        ~samples,  ~genes,
+             "A", "gene1",
+             "A", "gene2",
+             "B", "gene1",
+             "B", "gene3",
+             "C", "gene3"
     )
     k <- 2
     N_perms <- 6
+
+    expect_error(rc_test(dat, samples, genes, which_test = "fake_test"),
+                 regexp = "is not an option.")
+
     rc_res <- rc_test(dat, samples, genes,
                       k = k,
+                      which_test = "exclusivity",
                       N_perms = N_perms,
                       min_mutex_events = 0)
 
@@ -23,6 +28,45 @@ test_that("the RC-test works", {
     expect_true(all(lengths))
 
     expect_equal(unlist(rc_res$t_AM), c(1, 2, 3))
+
+    pval_check <- rc_res %>%
+        mutate(pval_check = (t_BM_ge / !!N_perms) == p_val) %>%
+        pull(pval_check) %>%
+        unlist()
+    expect_true(all(pval_check))
+})
+
+
+test_that("the RC-Co-mutation test works", {
+    library(dplyr)
+    dat <- tibble::tribble(
+        ~samples,  ~genes,
+             "A", "gene1",
+             "A", "gene2",
+             "B", "gene1",
+             "B", "gene3",
+             "C", "gene3"
+    )
+    k <- 2
+    N_perms <- 6
+
+    expect_error(rc_test(dat, samples, genes, which_test = "fake_test"),
+                 regexp = "is not an option.")
+
+    rc_res <- rc_test(dat, samples, genes,
+                      k = k,
+                      which_test = "comutation",
+                      N_perms = N_perms,
+                      min_mutex_events = 0)
+
+    expect_true(tibble::is_tibble(rc_res))
+    expect_true(ncol(rc_res) == 4)
+
+    gene_sets <- rc_res$gene_sets
+    lengths <- purrr::map_lgl(gene_sets, ~ length(.x) == 2)
+    expect_true(all(lengths))
+
+    expect_equal(unlist(rc_res$t_AM), c(1, 1))
 
     pval_check <- rc_res %>%
         mutate(pval_check = (t_BM_ge / !!N_perms) == p_val) %>%
